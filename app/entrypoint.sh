81962dfc769bd3b5b7e7438a21272b0ff536b417
Patrick Taylor Ramsey <ptr@fivestars.com>
2020-03-26 11:58:34 -0700

    [ALDN-142] Port to the python:3.8-slim image (#9)
    * [ALDN-142] Port to the python:3.8-slim image
    
    ... which fivestars is now standardizing on for python projects.  Projects
    using other languages should ideally use the official docker image for
    their language.
    
    [ALDN-142](https://fivestars.atlassian.net/browse/ALDN-142)
    
    * Revert "Use nfs mounts (#14)"
    
    Unless abhi has a reason to keep this as is.  Right now, this isn't the
    pattern we're following in other projects, and as it is it also embeds
    local minikube configuration details that may vary from host to host in
    its helm chart (specifically, the minikube ip and the fact that nfs is
    configured).
    
    This reverts commit b73facc031613d5b48cdd3f4c47be425f8dbdef5.
    
    * Add autoreload
    
    * Add back in withMount for commands pod
    
    * Move pip3 list to after packages are actually in place.  d'oh.
    
    * Restore old language
    
    * Add more documentation to new Dockerfile explaining sections
    
    * Clarify comment about installing C build environment

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,8 +3,8 @@
 if $APM_ENABLED; then
     echo "APM tracing enabled: $DATADOG_SERVICE_NAME"
     # start our server through ddtrace-run, which will allow us to see apm tracing in datadog
-    ddtrace-run uwsgi /config/uwsgi.yaml
+    exec ddtrace-run uwsgi /config/uwsgi.yaml
 else
     echo "APM tracing disabled"
-    uwsgi /config/uwsgi.yaml
+    exec uwsgi /config/uwsgi.yaml
 fi
